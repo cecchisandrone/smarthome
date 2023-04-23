680ed3c00be54b5ab6915ab90224da2ec567ab3b
Alessandro Dionisi <cecchisandrone@gmail.com>
2023-04-23 14:37:44 +0200

    Implemented WebDAV and improved influxdb performances

--- up-dev.sh
+++ up-dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Starts all the services passed as arguments
-exec docker-compose --project-name pi -f docker-compose.yml -f docker-compose.dev.yml up --build $@
+exec docker compose --project-name pi -f docker-compose.yml -f docker-compose.dev.yml up --build $@
